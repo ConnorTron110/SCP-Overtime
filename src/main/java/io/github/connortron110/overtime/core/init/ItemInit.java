063c0874ce9eb29161f1926992183c6e3ef6c28f
ConnorTron110 <connorcrook1912@gmail.com>
2021-05-16 15:27:06 +0100

    Changed SCP-714 ItemGroup

--- src/main/java/io/github/connortron110/overtime/core/init/ItemInit.java
+++ src/main/java/io/github/connortron110/overtime/core/init/ItemInit.java
@@ -18,9 +18,10 @@ public class ItemInit {
     public static final RegistryObject<Item> ICON_EUCLID = ITEMS.register("icon_euclid", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> ICON_KETER = ITEMS.register("icon_keter", () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<SCP714Item> SCP714 = ITEMS.register("scp714", () -> new SCP714Item(new Item.Properties().stacksTo(1).tab(Overtime.TAB_ITEMS)));
     public static final RegistryObject<Item> CUPCAKE = ITEMS.register("cupcake", () -> new Item(new Item.Properties().tab(Overtime.TAB_ITEMS).stacksTo(16).rarity(Rarity.COMMON).food(new Food.Builder().nutrition(4).saturationMod(0.3F).build())));
     public static final RegistryObject<SwordItem> MOB_DESPAWNER = ITEMS.register("mob_despawner", () -> new SwordItem(ItemTier.NETHERITE, 69420, 42069, new Item.Properties().tab(Overtime.TAB_ITEMS).fireResistant()));
+
+    public static final RegistryObject<SCP714Item> SCP714 = ITEMS.register("scp714", () -> new SCP714Item(new Item.Properties().stacksTo(1).tab(Overtime.TAB_MISC)));
 
     public static final RegistryObject<SpawnEggItem> SCP066_SPAWN_EGG = ITEMS.register("scp066_spawn_egg", () -> new ModSpawnEggItem(EntityInit.ERICS_TOY, new Color(0xC90000).getRGB(), new Color(0x650101).getRGB()));
 }
