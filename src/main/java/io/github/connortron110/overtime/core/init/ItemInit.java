908efd6c258712bda2a5c1b2c735bcd6c08c68df
ConnorTron110 <connorcrook1912@gmail.com>
2021-05-31 17:35:47 +0100

    Removed Unneeded boolean

--- src/main/java/io/github/connortron110/overtime/core/init/ItemInit.java
+++ src/main/java/io/github/connortron110/overtime/core/init/ItemInit.java
@@ -48,5 +48,5 @@ public class ItemInit {
 
     public static final RegistryObject<SCP035SpawnerItem> SCP035_TENDRIL_SPAWNER = ITEMS.register("scp035_tendril_spawner", () -> new SCP035SpawnerItem(EntityInit.SCP035_TENDRIL));
     public static final RegistryObject<SpawnEggItem> SCP066_SPAWN_EGG = ITEMS.register("scp066_spawn_egg", () -> new ModSpawnEggItem(EntityInit.SCP066, new Color(0xC90000).getRGB(), new Color(0x650101).getRGB()));
-    public static final RegistryObject<SpawnEggItem> SCP427_SPAWN_EGG = ITEMS.register("scp427_spawn_egg", () -> new ModSpawnEggItem(EntityInit.SCP427, new Color(0xFFE9CDCA, true).getRGB(), new Color(0xC69189).getRGB()));
+    public static final RegistryObject<SpawnEggItem> SCP427_SPAWN_EGG = ITEMS.register("scp427_spawn_egg", () -> new ModSpawnEggItem(EntityInit.SCP427, new Color(0xE9CDCA).getRGB(), new Color(0xC69189).getRGB()));
 }
