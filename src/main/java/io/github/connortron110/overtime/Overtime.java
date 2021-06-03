faf0f645032be0a64a6456392c0582c8fbfb2f11
ConnorTron110 <connorcrook1912@gmail.com>
2021-06-03 22:53:31 +0100

    Moved Attribute Creation Event Felt more convenient to have it within the EntityInit Class

--- src/main/java/io/github/connortron110/overtime/Overtime.java
+++ src/main/java/io/github/connortron110/overtime/Overtime.java
@@ -4,15 +4,11 @@ import io.github.connortron110.overtime.core.init.BlockInit;
 import io.github.connortron110.overtime.core.init.EntityInit;
 import io.github.connortron110.overtime.core.init.ItemInit;
 import io.github.connortron110.overtime.core.init.ModSounds;
-import net.minecraft.entity.MobEntity;
-import net.minecraft.entity.ai.attributes.AttributeModifierMap;
-import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -59,31 +55,9 @@ public class Overtime {
         ModSounds.SOUNDS.register(bus);
 
         bus.addListener(this::setup);
-        bus.addListener(this::onEntityAttributeCreation);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-    }
-
-    private void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
-        event.put(EntityInit.SCP035_TENDRIL.get(), createBaseAttributes(20, 0, 4,2).add(Attributes.KNOCKBACK_RESISTANCE, 1000).build());
-        event.put(EntityInit.SCP066.get(), createBaseAttributes(66, .2, 1,2).build());
-        event.put(EntityInit.SCP066_CAT.get(), createBaseAttributes(66, .2, 0,0).build());
-        event.put(EntityInit.SCP427.get(), createBaseAttributes(800, .4, 6,10).build());
-        event.put(EntityInit.SCP1762.get(), createBaseAttributes(2, .3, 3,2).add(Attributes.FLYING_SPEED, .3).build());
-        event.put(EntityInit.SCP3199.get(), createBaseAttributes(450, .4, 5, 1).build());
-        event.put(EntityInit.SCP3199_EGG.get(), createBaseAttributes(1000, 0, 0, 0).build());
-        event.put(EntityInit.SCP3456.get(), createBaseAttributes(750, .3, 9, 2).build());
-        event.put(EntityInit.SCP5167.get(), createBaseAttributes(50, .4, 3, 1).build());
-    }
-
-    private static AttributeModifierMap.MutableAttribute createBaseAttributes(double health, double moveSpeed, double attackDamage, double attackKnockback) {
-        return MobEntity.createLivingAttributes()
-                .add(Attributes.MAX_HEALTH, health)
-                .add(Attributes.FOLLOW_RANGE, 32)
-                .add(Attributes.MOVEMENT_SPEED, moveSpeed)
-                .add(Attributes.ATTACK_DAMAGE, attackDamage)
-                .add(Attributes.ATTACK_KNOCKBACK, attackKnockback);
     }
 }
--- src/main/java/io/github/connortron110/overtime/core/init/EntityInit.java
+++ src/main/java/io/github/connortron110/overtime/core/init/EntityInit.java
@@ -5,12 +5,19 @@ import io.github.connortron110.overtime.common.entities.projectile.AcidSpitEntit
 import io.github.connortron110.overtime.common.entities.scp.*;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 @SuppressWarnings("unchecked")
+@Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EntityInit {
     public static DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, Reference.MOD_ID);
 
@@ -25,6 +32,28 @@ public class EntityInit {
     public static final RegistryObject<EntityType<SCP3199EggEntity>> SCP3199_EGG = register("scp3199_egg", SCP3199EggEntity::new, EntityClassification.CREATURE, .5F, .5F);
     public static final RegistryObject<EntityType<SCP3456Entity>> SCP3456 = register("scp3456", SCP3456Entity::new, EntityClassification.MONSTER, 2.5F, 5F);
     public static final RegistryObject<EntityType<SCP5167Entity>> SCP5167 = register("scp5167", SCP5167Entity::new, EntityClassification.MONSTER, .6F, 1F);
+
+    @SubscribeEvent
+    public static void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
+        event.put(EntityInit.SCP035_TENDRIL.get(), createBaseAttributes(20, 0, 4,2).add(Attributes.KNOCKBACK_RESISTANCE, 1000).build());
+        event.put(EntityInit.SCP066.get(), createBaseAttributes(66, .2, 1,2).build());
+        event.put(EntityInit.SCP066_CAT.get(), createBaseAttributes(66, .2, 0,0).build());
+        event.put(EntityInit.SCP427.get(), createBaseAttributes(800, .4, 6,10).build());
+        event.put(EntityInit.SCP1762.get(), createBaseAttributes(2, .3, 3,2).add(Attributes.FLYING_SPEED, .3).build());
+        event.put(EntityInit.SCP3199.get(), createBaseAttributes(450, .4, 5, 1).build());
+        event.put(EntityInit.SCP3199_EGG.get(), createBaseAttributes(1000, 0, 0, 0).build());
+        event.put(EntityInit.SCP3456.get(), createBaseAttributes(750, .3, 9, 2).build());
+        event.put(EntityInit.SCP5167.get(), createBaseAttributes(50, .4, 3, 1).build());
+    }
+
+    private static AttributeModifierMap.MutableAttribute createBaseAttributes(double health, double moveSpeed, double attackDamage, double attackKnockback) {
+        return MobEntity.createLivingAttributes()
+                .add(Attributes.MAX_HEALTH, health)
+                .add(Attributes.FOLLOW_RANGE, 32)
+                .add(Attributes.MOVEMENT_SPEED, moveSpeed)
+                .add(Attributes.ATTACK_DAMAGE, attackDamage)
+                .add(Attributes.ATTACK_KNOCKBACK, attackKnockback);
+    }
 
     @SuppressWarnings("rawtypes")
     private static RegistryObject register(String name, EntityType.IFactory factory, EntityClassification classification, float width, float height) {
