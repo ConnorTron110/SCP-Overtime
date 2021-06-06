df51198b2b3beb7ffc57c05321dda133734a6ccb
ConnorTron110 <connorcrook1912@gmail.com>
2021-06-07 00:29:01 +0100

    Additional Glow Code Allows for same texture with different Glow variants

--- src/main/java/io/github/connortron110/overtime/client/render/entity/layers/GlowLayer.java
+++ src/main/java/io/github/connortron110/overtime/client/render/entity/layers/GlowLayer.java
@@ -13,13 +13,21 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
 public class GlowLayer<T extends Entity, M extends EntityModel<T>> extends LayerRenderer<T, M> {
+
+    private String variant = "";
+
     public GlowLayer(IEntityRenderer<T, M> iEntityRenderer) {
         super(iEntityRenderer);
     }
 
+    public void setVariant(int variant) {
+        this.variant = String.valueOf(variant);
+    }
+
     @Override
     public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        String glowLocation = getTextureLocation(entity).getPath().replace(".png", "_glow.png");
+        String replacement = "_glow" + variant + ".png";
+        String glowLocation = getTextureLocation(entity).getPath().replace(".png", replacement);
         IVertexBuilder ivertexbuilder = bufferIn.getBuffer(RenderType.eyes(new ResourceLocation(Overtime.MOD_ID, glowLocation)));
         getParentModel().renderToBuffer(matrixStackIn, ivertexbuilder, 15728640, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
     }
