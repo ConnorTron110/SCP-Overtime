2730b5ed90469fda59138f10b072a77de68fdd91
ConnorTron110 <connorcrook1912@gmail.com>
2021-07-01 02:09:05 +0100

    Added CommonEvent Class

---
+++ src/main/java/io/github/connortron110/overtime/common/CommonEvents.java
@@ -0,0 +1,8 @@
+package io.github.connortron110.overtime.common;
+
+import io.github.connortron110.overtime.Overtime;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Overtime.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class CommonEvents {
+}
