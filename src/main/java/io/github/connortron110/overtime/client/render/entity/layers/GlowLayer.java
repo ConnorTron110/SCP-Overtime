a4d80e34263108049e35769ce96a3b8624b1942f
ConnorTron110 <connorcrook1912@gmail.com>
2021-07-06 22:17:12 +0100

    Added ability to have 1 Glow layer for multiple textures

--- src/main/java/io/github/connortron110/overtime/client/render/entity/layers/GlowLayer.java
+++ src/main/java/io/github/connortron110/overtime/client/render/entity/layers/GlowLayer.java
@@ -15,6 +15,7 @@ import net.minecraft.util.ResourceLocation;
 public class GlowLayer<T extends Entity, M extends EntityModel<T>> extends LayerRenderer<T, M> {
 
     private String variant = "";
+    private int substring = 0;
 
     public GlowLayer(IEntityRenderer<T, M> iEntityRenderer) {
         super(iEntityRenderer);
@@ -23,12 +24,16 @@ public class GlowLayer<T extends Entity, M extends EntityModel<T>> extends Layer
     public void setVariant(int variant) {
         this.variant = String.valueOf(variant);
     }
+    public void setSubstring(int substring) {
+        this.substring = substring;
+    }
 
     @Override
     public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        String replacement = "_glow" + variant + ".png";
-        String glowLocation = getTextureLocation(entity).getPath().replace(".png", replacement);
-        IVertexBuilder ivertexbuilder = bufferIn.getBuffer(RenderType.eyes(new ResourceLocation(Overtime.MOD_ID, glowLocation)));
+        String glowLoc = "_glow" + variant + ".png";
+        String baseTexLoc = getTextureLocation(entity).getPath().replace(".png", "");
+        String texLocation = baseTexLoc.substring(0, baseTexLoc.length() - substring) + glowLoc;
+        IVertexBuilder ivertexbuilder = bufferIn.getBuffer(RenderType.eyes(new ResourceLocation(Overtime.MOD_ID, texLocation)));
         getParentModel().renderToBuffer(matrixStackIn, ivertexbuilder, 15728640, OverlayTexture.NO_OVERLAY, 1F, 1F, 1F, 1F);
     }
 }
