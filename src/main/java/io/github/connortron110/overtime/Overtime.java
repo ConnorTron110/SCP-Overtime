d69dd775eb90de0fda6741b2b00bd4f0a9b45135
ConnorTron110 <connorcrook1912@gmail.com>
2021-07-17 12:37:37 +0100

    Misc Main File Cleanup

--- src/main/java/io/github/connortron110/overtime/Overtime.java
+++ src/main/java/io/github/connortron110/overtime/Overtime.java
@@ -6,8 +6,6 @@ import io.github.connortron110.overtime.core.init.ModItems;
 import io.github.connortron110.overtime.core.init.ModSounds;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -23,30 +21,26 @@ public class Overtime {
     public static final Logger LOGGER = LogManager.getLogger("Overtime");
 
     //Item Groups
-    public static final ItemGroup TAB_ITEMS = (new ItemGroup(ItemGroup.TABS.length, "overtimeitems") {
-        @OnlyIn(Dist.CLIENT)
+    public static final ItemGroup TAB_ITEMS = new ItemGroup(ItemGroup.TABS.length, "overtimeitems") {
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.ICON_EUCLID.get());
         }
-    });
-    public static final ItemGroup TAB_BLOCKS = (new ItemGroup(ItemGroup.TABS.length, "overtimeblocks") {
-        @OnlyIn(Dist.CLIENT)
+    };
+    public static final ItemGroup TAB_BLOCKS = new ItemGroup(ItemGroup.TABS.length, "overtimeblocks") {
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.ICON_SAFE.get());
         }
-    });
-    public static final ItemGroup TAB_MOBS = (new ItemGroup(ItemGroup.TABS.length, "overtimemobs") {
-        @OnlyIn(Dist.CLIENT)
+    };
+    public static final ItemGroup TAB_MOBS = new ItemGroup(ItemGroup.TABS.length, "overtimemobs") {
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.ICON_KETER.get());
         }
-    });
-    public static final ItemGroup TAB_MISC = (new ItemGroup(ItemGroup.TABS.length, "overtimemisc") {
-        @OnlyIn(Dist.CLIENT)
+    };
+    public static final ItemGroup TAB_MISC = new ItemGroup(ItemGroup.TABS.length, "overtimemisc") {
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.ICON_SAFE.get());
         }
-    });
+    };
 
     public Overtime() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
